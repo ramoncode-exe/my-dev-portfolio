ddad0f97e85a0972f62a1731e2be8fbed09bb857
ramoncode-exe <ramonpatrick0906@gmail.com>
2025-05-20 19:55:34 +0100

    Section - Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,7 @@
 import { motion } from "framer-motion";
+import LogoAzure from "../assets/azure-project.png";
+import ToDoLoGo from "../assets/todo-project.png";
+import YourCoffeLogo from "../assets/yourcoffe-project.png";
 
 function Projects(){
     return(
@@ -7,9 +10,9 @@ function Projects(){
                 <h1 className="text-3xl font-bold mb-5">My <span className="text-[#fb5607]">Projects</span></h1>
             </div>
             <div className="flex flex-row items-center justify-center max-lg:flex-wrap gap-8">
-            <motion.div className="border p-3 border-[#cccccc20] w-100 max-md:w-80">
-                    <div>
-                        <img src="" alt="" />   
+            <motion.div initial={{ y: +300, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 4, ease: "easeIn" }} className="border p-3 border-[#cccccc20] w-100 max-md:w-80">
+                    <div className="flex items-center justify-center">
+                        <img className="w-60 mt-2 mb-2" src={ToDoLoGo} alt="todo logo" />   
                     </div>
                     <div>
                         <h1 className="mt-3 text-2xl font-bold">ToDo Lists</h1>
@@ -25,9 +28,9 @@ function Projects(){
                         <motion.button onClick={() => window.open("https://to-do-html-css-and-js.vercel.app/")} target="_blank" whileHover={{ scale: 1.1 }} className="border border-[#fb560770] p-2 hover:bg-[#fb5607]">View Project</motion.button>
                     </div>
             </motion.div>
-            <motion.div className="border p-3 border-[#cccccc20] w-100 max-md:w-80">
-                    <div>
-                        <img src="" alt="" />   
+            <motion.div initial={{ y: +300, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 4, ease: "easeIn" }} className="border p-3 border-[#cccccc20] w-100 max-md:w-80">
+                    <div className="flex items-center justify-center">
+                        <img className="w-60 mt-2 mb-2" src={YourCoffeLogo} alt="Coffe logo" />   
                     </div>
                     <div>
                         <h1 className="mt-3 text-2xl font-bold">Your Coffe</h1>
@@ -43,9 +46,9 @@ function Projects(){
                         <motion.button onClick={() => window.open("https://to-do-html-css-and-js.vercel.app/")} target="_blank" whileHover={{ scale: 1.1 }} className="border border-[#fb560770] p-2 hover:bg-[#fb5607]">View Project</motion.button>
                     </div>
             </motion.div>
-            <motion.div className="border p-3 border-[#cccccc20] w-100 max-md:w-80">
-                    <div>
-                        <img src="" alt="" />
+            <motion.div initial={{ y: +300, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 4, ease: "easeIn" }} className="border p-3 border-[#cccccc20] w-100 max-md:w-80">
+                    <div className="flex items-center justify-center">
+                        <img className="w-60 mt-2 mb-2" src={LogoAzure} alt="azure logo" />
                     </div>
                     <div>
                         <h1 className="mt-3 text-2xl font-bold">Azure</h1>
