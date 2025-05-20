27cb3a65900a9d49384e95a51e842c415cb85fb9
ramoncode-exe <ramonpatrick0906@gmail.com>
2025-05-20 02:21:40 +0100

    Responsive Updates - Section About Me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ function About(){
             </div>
             <div className="p-3 border border-[#fb560730] rounded-lg max-lg:mt-8">
                 <h2 className="text-3xl font-bold mb-5">About <span className="text-[#fb5607]">Me</span></h2>
-                <p className="w-120 text-md mb-5 max-lg:w-100">I’m a front-end developer focused on building modern, responsive, and conversion-driven interfaces. <span className="text-[#fb5607]">Skilled in HTML, CSS, JavaScript, React, Tailwind, and Bootstrap.</span> I deliver clean, scalable, and user-friendly solutions that blend design and performance.</p>
+                <p className="w-120 text-md mb-5 max-lg:w-100 max-sm:w-70">I’m a front-end developer focused on building modern, responsive, and conversion-driven interfaces. <span className="text-[#fb5607]">Skilled in HTML, CSS, JavaScript, React, Tailwind, and Bootstrap.</span> I deliver clean, scalable, and user-friendly solutions that blend design and performance.</p>
                 <button className="bg-[#fb560750] p-2 mr-5 hover:bg-transparent border border-[#fb5607]">Contact Me</button>
             </div>
         </div>
